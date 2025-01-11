4848300736bc064fc65a20e9cd8147d2896c6af2
Utkarsh <utkarshjha.4009@gmail.com>
2025-01-12 00:25:48 +0530

    Fixes

--- models/orderConfirmedModel.js
+++ models/orderConfirmedModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const NotificationSchema = new mongoose.Schema({
   id: { type: String, required: true },
   orderno: { type: String, required: true },
-  message: { type: String, required: true },
+  message: { type: String, required: false },
   username: { type: String, required: true },
   isRead: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
